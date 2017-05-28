068621371306b1ad5d990ba11187fbb4e0cab061
Syed Akber Jafri <Syedjafri99@gmail.com>
2017-05-27 23:49:03 -0400

    executing tempScripts properly

--- postinstall.js
+++ postinstall.js
@@ -1,2 +1,3 @@
 // TODO temp solution
-exec("psql -f tempScripts.sql" + process.env.DATABASE_URL);
+var exec = require('child_process').exec;
+exec("psql -f ./tempScripts.sql" + process.env.DATABASE_URL ,(error, stdout, stderr) => {console.log(error, stdout, stderr)});

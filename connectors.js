56f225543e131395fa41ee27600e0bf7c048a4d3
Syed Akber Jafri <Syedjafri99@gmail.com>
2017-05-27 23:28:47 -0400

    Using heroku db if on heroku

--- connectors.js
+++ connectors.js
@@ -1,19 +1,29 @@
 import Sequalize from 'sequelize';
 import _ from 'lodash';
 
-// TODO specify schema public
-// Don't worry about the credentials here. They will change later. Just using for local now.
-const db = new Sequalize('example', 'postgres', 'admin', {
+const dbOptions =  {
     dialect: 'postgres',
-    host: 'localhost',
-    port: '5432',
+    protocol: 'postgres',
     define: {
         schema: "public",
         freezeTableName: true,
         createdAt: "created_at",
         updatedAt: "updated_at",
     },
-});
+};
+
+let db;
+if (process.env.DATABASE_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    db = new Sequalize(process.env.DATABASE_URL, dbOptions);
+} else {
+    // the application is executed on the local machine
+    db = new Sequalize("example", "postgres", 'admin', {
+        host: 'localhost',
+        port: '5432',
+        ...dbOptions,
+    });
+}
 
 const UserModel = db.define('user', {
     id: { type: Sequalize.INTEGER, primaryKey: true },

682c612f59d1c3cf6728dcb22f4b9953c007fdfe
Syed Akber Jafri <Syedjafri99@gmail.com>
2017-05-27 23:50:30 -0400

    😡  forgot a space

--- postinstall.js
+++ postinstall.js
@@ -1,3 +1,3 @@
 // TODO temp solution
 var exec = require('child_process').exec;
-exec("psql -f ./tempScripts.sql" + process.env.DATABASE_URL ,(error, stdout, stderr) => {console.log(error, stdout, stderr)});
+exec("psql -f ./tempScripts.sql " + process.env.DATABASE_URL ,(error, stdout, stderr) => {console.log(error, stdout, stderr)});

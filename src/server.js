1a757c8865eaceeae2945a61a9b9716e5bc30224
Rob <milrober@gmail.com>
2017-07-08 17:54:19 -0400

    Configure socket connection to send tweets from the server to client

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 // @flow
 import fs from 'fs';
-
+import http from 'http';
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
+import SocketIO from 'socket.io';
 import ini from 'ini';
 import Twitter from 'twitter';
 import { 
@@ -16,10 +18,32 @@ import { schema } from './schema.js';
 const config = ini.parse(fs.readFileSync('./local.ini', 'utf-8'));
 const PORT = config.server.PORT || 4000;
 const server = express();
+const httpServer = http.createServer(server);
+
+server.use(express.static('src'));
+
+server.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema
+}));
+
+server.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql'
+}));
+
+server.get('/twitter', (request, response) => {
+    response.sendFile(path.join(__dirname, 'debug.html'));
+});
+
+// Serve via http server to enable web socket connections
+httpServer.listen(PORT, () => 
+  console.log(`GraphQL Server is now running on http://localhost:${PORT}`)
+);
 
 
+const io = SocketIO(httpServer);
+
+// Setup a twitter client with credentials defined in local.ini
 const tc = config.twitter_credentials;
-
 const twitterClient = new Twitter({
     consumer_key: tc.consumer_key,
     consumer_secret: tc.consumer_secret,
@@ -27,6 +51,7 @@ const twitterClient = new Twitter({
     access_token_secret: tc.access_token_secret
 });
 
+// Food truck twitter IDs used to specify which tweets we want to read from
 const FoodTrucksTwitterIDs = {
     ChickfilaMobile: "540537070",
     tropicburger: "722571703008825344",
@@ -41,32 +66,21 @@ const FoodTrucksTwitterIDs = {
     sanityCheck2: "8453452"
 };
 
-
-server.use('/graphql', bodyParser.json(), graphqlExpress({
-  schema
-}));
-
-server.use('/graphiql', graphiqlExpress({
-  endpointURL: '/graphql'
-}));
+// Specify params to send to our request
+const streamParams = { follow: Object.values(FoodTrucksTwitterIDs).join(',') };
 
 
-let stream = twitterClient.stream(
-    'statuses/filter',
-    {
-        follow: Object.values(FoodTrucksTwitterIDs).join(',')
-    }
-);
-stream.on('data', event => {
-    console.log(event && event.text);
+// Once a socket connection has been established...
+io.on('connection', socket => {
+
+    // Start streaming the twitter statuses of our specified food trucks
+    twitterClient.stream('statuses/filter', streamParams, stream => {
+            stream.on('data', event => {
+                // When we receive a tweet, send it to the client side
+                socket.emit('tweet', { event, content: event.text });
+            });
+            stream.on('error', e => console.error(e));
+        }
+    );
+
 });
-stream.on('error', e => console.error(e));
-
-
-server.get('/twitter', (request, response) => {
-    response.send("Foo");
-});
-
-server.listen(PORT, () => 
-  console.log(`GraphQL Server is now running on http://localhost:${PORT}`)
-);
